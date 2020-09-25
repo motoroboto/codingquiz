21776f13c1662eacfa9ea1320dcf9fef3569ff26
motoroboto <robert.a.cullen@gmail.com>
2020-09-25 16:48:34 -0400

    basic script to start timer

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+var timeEl = document.getElementById('timer');
+var question = document.getElementById('question');
+var answers = document.getElementById('answers');
+var results = document.getElementById('results');
+// var startquiz = document.getElementById('startquiz')
+
+var secondsLeft = 60;
+
+function quizBegin() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+function sendMessage() {
+  timeEl.textContent = " ";
+
+}
+
+document.getElementById('startquiz').addEventListener('click', quizBegin);
