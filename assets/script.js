583c222dbca9b25fefd88a90bac6ba8167386887
motoroboto <robert.a.cullen@gmail.com>
2020-09-25 20:40:06 -0400

    added array of questions

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,80 @@ var answersEl = document.getElementById('answers');
 var resultsEl = document.getElementById('results');
 var greetingText = document.getElementById('greeting');
 var secondsLeft = 60;
+var progress = 0;
+var score = 0;
+
+
+var questions = [
+{
+    question: "what is java?",
+    a: "coffee",
+    b: "a coding language",
+    c: "a color"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+},
+{
+    question: "what is HTML?",
+    a: "hyper text markup language",
+    b: "have the most laughs",
+    c: "his time may last"
+}
+];
+
+
 
 function quizBegin() {
     greetingText.innerHTML = '';
@@ -23,5 +97,4 @@ function sendMessage() {
   timeEl.textContent = '';
 
 }
-
 document.getElementById('startquiz').addEventListener('click', quizBegin);
