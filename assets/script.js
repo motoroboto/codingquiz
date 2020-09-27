c0b86bd21892bbc4eb0a199b64d36a373974694c
motoroboto <robert.a.cullen@gmail.com>
2020-09-26 23:04:55 -0400

    running loop thru quiz questions, still fixing some bugs tho

--- assets/script.js
+++ assets/script.js
@@ -2,67 +2,81 @@ var timeEl = document.getElementById('timer');
 var questionEl = document.getElementById('question');
 var resultsEl = document.getElementById('results');
 var greetingText = document.getElementById('greeting');
+var questionHeader = document.getElementById('questionheader')
 var secondsLeft = 60;
 var progress = 0;
-var score = 0;
-var questionHeader = document.getElementById('questionheader')
 
 
-
-var question01 = {
+var questions = [
+  {
     question: "What is java?",
     answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"],
     correct: 1
-}
-var question02 = {
-  question: "What is java?",
-  answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"]
-}
-var question03 = {
-  question: "What is java?",
-  answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"]
-}
-var question04 = {
-  question: "What is java?",
-  answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"]
-}
-var question05 = {
-  question: "What is java?",
-  answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"]
-}
-
+    },
+  {
+    question: "What else is java?",
+    answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"],
+    correct: 2
+    },
+  {
+    question: "Something Else?",
+    answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"],
+    correct: 3
+    },
+  {
+    question: "Another java thing?",
+    answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"],
+    correct: 0
+    },
+  {
+    question: "What isn't again?",
+    answers: ["A: Coffee", "B: A Coding Language", "C: A Color", "D: some other option"],
+    correct: 3
+    }
+]
 
 function quiz() {
-        
-  questionHeader.textContent = question01.question;
-  var answers = question01.answers;
-  for (var i = 0; i < answers.length; i++) {
-      var li = document.createElement("li");
-      li.setAttribute("class", "button btn");
-      li.setAttribute("id", [i]);
+      cq = progress;  
+  console.log('progress:', progress);
+  console.log('currentquestion:', cq);
+  questionHeader.textContent = questions[cq].question;
+  console.log('textContent:', questionHeader.textContent);
+  console.log('question:', questions[cq].question);
+  for (var i = 0; i < 4; i++) {
+      var li = document.createElement('li');
+      li.setAttribute('class', 'button btn');
+      li.setAttribute('id', [i]);
       console.log('i:', li.id)
-
-      li.textContent = answers[i];
-      var br = document.createElement("br");
+      li.textContent = questions[cq].answers[i];
+      var br = document.createElement('br');
       questionEl.appendChild(li);
       questionEl.appendChild(br);
   };  
 };
-questionEl.addEventListener("click", function(event) {
+
+questionEl.addEventListener('click', function(event) {
   var target = event.target;
   console.log('target:', target)
-  if (target.id == question01.correct) {
-    console.log('correct:', question01.correct)
+  if (target.id == questions[cq].correct) {
+    console.log('correct:', questions[cq].correct)
     console.log('id:', target.id)
-    resultsEl.textContent = "Good Job"; 
+    resultsEl.textContent = 'Good Job'; 
+    progress = progress +1;
+    console.log('progress:', progress);
+    questionEl.textContent = '';
+    questionHeader.textContent = '';
+    quiz()
+    setTimeout(function(){ 
+      resultsEl.textContent = '';
+  }, 1000);
   } else {
-    console.log('correct:', question01.correct)
+    console.log('correct:', questions[cq].correct)
     console.log('id:', target.id)
     secondsLeft = secondsLeft - 10;
     resultsEl.textContent = "You Lost 10 seconds, Hot Shot!";
-  // setTimeout(function(){
-  //   resultsEl.getElementById('check').innerHTML='';
-  //   },3000);
+    setTimeout(function(){ 
+      resultsEl.textContent = '';
+  }, 1000);
   }
 });
 
